2d30a88593f60b0ba66d25ca89c86689274326f4
yusuf <yusuf@DESKTOP-4BCDGTC>
2023-01-07 17:54:58 +0300

    Add project files.

---
+++ FACT2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FACT2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int givenNumber, factorial = 1;
+
+            Console.WriteLine("For Loop - Find the factorial of a given number :");
+            Console.WriteLine("///////////////////////////////////////////////////////");
+            givenNumber = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 2; i <= givenNumber; i++)
+            {
+                factorial*= i;
+            }
+
+            Console.WriteLine("Factorial : {0}", factorial);
+            Console.ReadKey();
+
+        }
+    }
+}
